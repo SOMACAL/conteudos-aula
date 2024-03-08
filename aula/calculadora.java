640882750096f11adbebe51b5375f44d7b257962
SOMACAL <105447484+SOMACAL@users.noreply.github.com>
2024-03-08 10:56:51 -0300

    Add files via upload

---
+++ aula/calculadora.java
@@ -0,0 +1,26 @@
+public class calculadora {
+    public static int somar(int a, int b) {
+        // int r = a + b;
+        // return r;
+        return a + b;
+    }
+
+    // subtrair
+    public static int subtrair(int a, int b) {
+        // int r = a - b;
+        // return r;
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        // int r = a * b;
+        // return r;
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        // int r = a / b;
+        // return r;
+        return a / b;
+    }
+}
